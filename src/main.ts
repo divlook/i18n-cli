bd828ede5b32fcbc18d2b15ade83759c5e96cc07
uihyeon.cha <uihyeon.cha@gmail.com>
2022-06-13 23:11:56 +0900

    feat: cli 코드 추가

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log('main')
+import { program } from 'commander'
+import { version } from '~/package.json'
+
+program
+    .name('divlook-i18n')
+    .version(version, '-v, --version', 'output the current version')
+    .option(
+        '-o -output <path>',
+        '번역 파일들이 저장될 경로 및 파일명. 변수 `column`, `sheet_name`을 사용할 수 있습니다. ex) "./i18n/[column]", "./i18n/[sheet_name]/[column]"',
+        './translations/[column]'
+    )
+
+program.parse(process.argv)
+
+// const options = program.opts()
