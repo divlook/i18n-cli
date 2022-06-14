00ef036bd15bbbe9c346744e8eb59dc339339fb4
uihyeon.cha <uihyeon.cha@gmail.com>
2022-06-14 22:31:21 +0900

    feat: cli option 구현

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,57 @@
 import { program } from 'commander'
 import packageJson from '~/package.json'
+import { OutputFormatEnum } from '~/src/types'
 
 program
-    .name('divlook-i18n')
-    .version(packageJson.version, '-v, --version', 'output the current version')
+    .name(`divlook-i18n`)
+    .version(packageJson.version, `-v, --version`, `output the current version`)
     .option(
-        '-o -output <path>',
-        '번역 파일들이 저장될 경로 및 파일명. 변수 `column`, `sheet_name`을 사용할 수 있습니다. ex) "./i18n/[column]", "./i18n/[sheet_name]/[column]"',
-        './translations/[column]'
+        `-o --output <path>`,
+        `번역 파일들이 저장될 경로 및 파일명. 변수 \`column\`, \`sheet_name\`을 사용할 수 있습니다. ex) \`./i18n/[column]\`, \`./i18n/[sheet_name]/[column]\``,
+        `./translations/[column]`
+    )
+    .option(
+        `--output-format <format>`,
+        `지원되는 포맷 : ${OutputFormatEnum.Json}`,
+        OutputFormatEnum.Json
+    )
+    .option(
+        `--clean`,
+        `결과물을 생성하기 전에 output 디렉토리를 깨끗하게 정리합니다.`,
+        false
+    )
+    .option(
+        `--exclude-columns <columns>`,
+        `결과물에서 제외시킬 컬럼명. \`,\`를 사용하여 복수로 입력할 수 있습니다. ex) \`desc\`, \`memo\` glob 패턴을 지원합니다. ex) \`ignore-*\``
+    )
+    .option(
+        `--exclude-keys <keys>`,
+        `결과물에서 제외시킬 key. \`,\`를 사용하여 복수로 입력할 수 있습니다. ex) \`key1\`, \`key2\` glob 패턴을 지원합니다. ex) \`ignore-*\``
+    )
+    .option(
+        `--include-sheets <sheets>`,
+        `변환을 시도할 시트명. \`,\`를 사용하여 복수로 입력할 수 있습니다. ex) \`Sheet 1\`, \`Sheet 2\` glob 패턴을 지원합니다. ex) \`Sheet*\``,
+        `*`
+    )
+    .option(`--input <path>`, `\`xlsx\`, csv 파일 허용. ex) \`./i18n.xlsx\``)
+    .option(
+        `--spreadsheet-id <id>`,
+        `ID 찾는 방법 : https://docs.google.com/spreadsheets/d/{{id}}/edit`
+    )
+    .option(
+        `--google-credentials <path>`,
+        `Google Sheets Node API credentials 파일의 경로`,
+        `./credentials.json`
+    )
+    .option(
+        `--google-token <path>`,
+        `Google Sheets API token 파일의 경로. 최초 실행시 생성되고 이후는 재사용할 수 있습니다.`,
+        `./token.json`
+    )
+    .option(
+        `--key-format <format>`,
+        `key 포맷입니다. 변수 \`key\`, \`sheet_name\`을 사용할 수 있습니다. ex) \`[key]\`, \`[sheet_name].[key]\``,
+        `[key]`
     )
 
 program.parse(process.argv)
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export const enum OutputFormatEnum {
+    Json = 'json',
+}
+
+export type OutputFormat = `${OutputFormatEnum}`
