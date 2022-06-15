e30e1176743c37ee9d4290edcef5aac25cb0b3a1
uihyeon.cha <uihyeon.cha@gmail.com>
2022-06-16 00:14:29 +0900

    feat: 커맨드 옵션 인터페이스 추가

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,17 @@ export const enum OutputFormatEnum {
 }
 
 export type OutputFormat = `${OutputFormatEnum}`
+
+export interface CommandOption {
+    output: string
+    outputFormat: OutputFormatEnum
+    clean: boolean
+    excludeColumns?: string
+    excludeKeys?: string
+    includeSheets: string
+    input?: string
+    spreadsheetId?: string
+    googleCredentials?: string
+    googleToken?: string
+    keyFormat?: string
+}
